31fd519525d420e4cf1d248c9b29d8e0d3ccaba0
Kacper Pietrusiak <kacperpietrusiak.biznes@gmail.com>
2025-06-05 19:31:12 +0200

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://mathbyte.pl',
+    generateRobotsTxt: true,
+    // Opcjonalnie: wyklucz ścieżki, ustaw częstotliwość aktualizacji itd.
+  }
+  
