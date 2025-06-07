f7cb9ab0ebb64ce5f7b9f8be7c0452b90d7aef05
Kacper Pietrusiak <kacperpietrusiak.biznes@gmail.com>
2025-06-07 13:38:58 +0200

    add blik to payment methods

--- app/api/checkout-session/route.ts
+++ app/api/checkout-session/route.ts
@@ -45,7 +45,7 @@ export async function POST(req: Request) {
 
   const session = await stripe.checkout.sessions.create({
     mode: "payment",
-    payment_method_types: ["card"],
+    payment_method_types: ["card", "blik"],
     customer_email: email,
     line_items: [
       {
